6cbea8514c87fa4cab7fceb165398a939bb88f01
Douglas Lowder <douglowder@mac.com>
2025-09-08 12:45:22 -0700

    Move to preview 16

--- layouts/TabLayout.web.tsx
+++ layouts/TabLayout.web.tsx
@@ -1,7 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform, Pressable } from 'react-native';
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { useTextStyles } from '@/hooks/useTextStyles';
